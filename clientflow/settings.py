4698a2c2734332a3f6af5cc9bb850319bfc7fb18
Lorenzo V <lorenzo.vella@live.com>
2020-07-21 22:19:11 -0300

    Database overload fix and model adjust

--- clientflow/settings.py
+++ clientflow/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'clientflow.wsgi.application'
 
 DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
