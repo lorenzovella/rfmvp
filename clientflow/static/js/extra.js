57ae9fa376bfd5990810c1b9d362902d4746f096
Lorenzo V <lorenzo.vella@live.com>
2020-07-23 20:36:21 -0300

    implemented autocomplete from static json

--- clientflow/static/js/extra.js
+++ clientflow/static/js/extra.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-  $('.control-group').last().css('margin-bottom','180px')
+  $('.control-group').last().css('margin-bottom','180px');
+  var arr = [];
+  $.getJSON("/static/racas.json", function(data) {
+      $.each(data, function(key, value) {
+          if ($.inArray(value.name, arr) === -1) {
+              arr.push(value.name)
+          }
+      })
+  });
+ $('#id_CachorroForm2-raca').autocomplete({
+   source: arr,
+   minLength: 2,
   })
+});
--- staticfiles/js/extra.420c3b0e3edb.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-  $('.control-group').last().css('margin-bottom','180px')
-  })
