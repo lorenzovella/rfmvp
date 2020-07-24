1b1d6384a4d796832e8b8854ffb7d5e8b157c8b3
Lorenzo V <lorenzo.vella@live.com>
2020-07-23 23:01:31 -0300

    mobile improvements

--- clientflow/static/js/extra.js
+++ clientflow/static/js/extra.js
@@ -12,4 +12,16 @@ $(document).ready(function () {
    source: arr,
    minLength: 2,
   })
+  let fullWindowHeight = window.innerHeight;
+  let keyboardIsProbablyOpen = false;
+
+  window.addEventListener("resize", function() {
+    if(window.innerHeight == fullWindowHeight) {
+      $('.footer').show()
+      keyboardIsProbablyOpen = false;
+    } else if(window.innerHeight < fullWindowHeight*0.9) {
+      $('.footer').hide()
+      keyboardIsProbablyOpen = true;
+    }
+  });
 });
