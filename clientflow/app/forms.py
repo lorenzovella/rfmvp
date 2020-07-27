e8276d054e846a64727f029402f5df4ea22a2359
Lorenzo V <lorenzo.vella@live.com>
2020-07-27 12:32:28 -0300

    improvements on pedidos page

--- clientflow/app/forms.py
+++ clientflow/app/forms.py
@@ -42,7 +42,7 @@ class EntregaForm3(forms.ModelForm):
         self.helper = FormHelper(self)
         self.helper.layout = Layout(
             HTML('<button name="wizard_goto_step" class="btn-back" type="submit" value="{{ wizard.steps.prev }}"><svg width="31px" height="22px" viewBox="0 0 31 22" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><path d="M11.2306 0.421231C11.7798 0.994106 11.7798 1.94508 11.2414 2.52941L4.70545 9.50702L29.611 9.50702C30.3755 9.50702 31 10.1716 31 10.9965C31 11.8214 30.3755 12.486 29.611 12.486L4.70545 12.486L11.2522 19.4636C11.7906 20.0479 11.7798 20.9874 11.2414 21.5718C10.6923 22.1446 9.82008 22.1446 9.27093 21.5603L0.398402 12.0506C0.279958 11.9131 0.18305 11.7642 0.107676 11.5808C0.0323029 11.3975 0 11.2027 0 11.008C0 10.6184 0.139979 10.2518 0.398402 9.96532L9.27093 0.455604C9.79854 -0.140186 10.6815 -0.151643 11.2306 0.421231L11.2306 0.421231Z" id="Path" fill="#17437A" stroke="none" /> </svg></button>'),
-            HTML(' <p> {{dia|join:", "}} | {{periodo|join:", "}}'),
+            HTML('<div class="card"><div class="card-body"><p class="card-heading">Próxima entrega</p><p class="card-text">{{dia|join:", "}}</p><p class="card-text">{{periodo|join:", "}}</p></div></div>'),
             InlineRadios('preferencia'),
             InlineRadios('frequencia'),
             HTML('<div class="footer margin-footer"><input type="submit" class="btn-next" value="Continuar"/></div>'),
