0cd6e7041f4d1584887a179b93ca3527e6389a25
Lorenzo V <lorenzo.vella@live.com>
2020-08-01 05:34:44 -0300

    layout improvement

--- clientflow/static/js/extra.js
+++ clientflow/static/js/extra.js
@@ -5,16 +5,16 @@ $(document).ready(function () {
 
   let fullWindowHeight = window.innerHeight;
   let keyboardIsProbablyOpen = false;
-
-  window.addEventListener("resize", function() {
-    if(window.innerHeight == fullWindowHeight) {
-      $('.footer').show()
-      keyboardIsProbablyOpen = false;
-    } else if(window.innerHeight < fullWindowHeight*0.9) {
-      $('.footer').hide()
-      keyboardIsProbablyOpen = true;
-    }
-  });
+  // 
+  // window.addEventListener("resize", function() {
+  //   if(window.innerHeight == fullWindowHeight) {
+  //     $('.footer').show()
+  //     keyboardIsProbablyOpen = false;
+  //   } else if(window.innerHeight < fullWindowHeight*0.9) {
+  //     $('.footer').hide()
+  //     keyboardIsProbablyOpen = true;
+  //   }
+  // });
 
   $(document).keypress(
   function(event){
