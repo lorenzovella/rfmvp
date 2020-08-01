e1e9a47ed8c145f52b99860628abbcbc067cfabd
Lorenzo V <lorenzo.vella@live.com>
2020-08-01 04:23:25 -0300

    new css integrated to all user pages

--- clientflow/urls.py
+++ clientflow/urls.py
@@ -25,8 +25,13 @@ handler500 = 'clientflow.app.views.handler500'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', authentication_form=CustomAuthForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view()),
+    path('esqueciminhasenha/', auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('esqueciminhasenha/conf/', auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('esqueciminhasenha/done', auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('trocarsenha/', auth_views.PasswordResetCompleteView.as_view()),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('', include('clientflow.app.urls')),
     path('admin/', admin.site.urls),
     # path('accounts/', include('django.contrib.auth.urls')),
+
 ]
