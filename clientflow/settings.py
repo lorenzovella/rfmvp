f7bed82ef5971b7c766568d0698d0c4de6aed06c
Lorenzo V <lorenzo.vella@live.com>
2020-08-12 15:21:43 -0300

    Update settings.py

--- clientflow/settings.py
+++ clientflow/settings.py
@@ -148,5 +148,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'empresa@racaodofuturo.com.br'
 EMAIL_HOST_PASSWORD = os.environ.get('mailtoken')
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
