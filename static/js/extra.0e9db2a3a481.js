e31e1840278213f55db3ebc2825e209d82a2d85b
Lorenzo V <lorenzo.vella@live.com>
2020-08-13 10:10:46 -0300

    added static files

---
+++ static/js/extra.0e9db2a3a481.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+// Responsiveness helpers
+  // $('.control-group').last().css('margin-bottom','180px');
+
+  let fullWindowHeight = window.innerHeight;
+  let keyboardIsProbablyOpen = false;
+
+  $(document).keypress(
+  function(event){
+    if (event.which == '13') {
+      event.preventDefault();
+      $('.btn-next').trigger('click');
+      return false;
+    }
+  });
+});
