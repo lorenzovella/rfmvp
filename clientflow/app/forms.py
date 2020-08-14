1b9ba3875b5d11786c5433bbb3a23be47a6f3fe5
Lorenzo V <lorenzo.vella@live.com>
2020-08-14 17:25:24 -0300

    form flow improvements

--- clientflow/app/forms.py
+++ clientflow/app/forms.py
@@ -9,6 +9,8 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.forms import AuthenticationForm
 from django.forms.widgets import PasswordInput, TextInput
 
+footerHtml=Div(Submit("submit","Continuar",css_class="btn-next"), css_class="footer margin-footer")
+
 
 class CustomAuthForm(AuthenticationForm):
     class Meta:
@@ -83,8 +85,8 @@ class EntregaForm(forms.ModelForm):
         self.helper = FormHelper(self)
         self.helper.layout = Layout(
             HTML('<button name="wizard_goto_step" class="btn-back" type="submit" value="{{ wizard.steps.prev }}"><svg width="31px" height="22px" viewBox="0 0 31 22" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><path d="M11.2306 0.421231C11.7798 0.994106 11.7798 1.94508 11.2414 2.52941L4.70545 9.50702L29.611 9.50702C30.3755 9.50702 31 10.1716 31 10.9965C31 11.8214 30.3755 12.486 29.611 12.486L4.70545 12.486L11.2522 19.4636C11.7906 20.0479 11.7798 20.9874 11.2414 21.5718C10.6923 22.1446 9.82008 22.1446 9.27093 21.5603L0.398402 12.0506C0.279958 11.9131 0.18305 11.7642 0.107676 11.5808C0.0323029 11.3975 0 11.2027 0 11.008C0 10.6184 0.139979 10.2518 0.398402 9.96532L9.27093 0.455604C9.79854 -0.140186 10.6815 -0.151643 11.2306 0.421231L11.2306 0.421231Z" id="Path" fill="#17437A" stroke="none" /> </svg></button>'),
-            InlineCheckboxes('dia',css_class="custom-control-checkbox"),
-            HTML('<div class="footer margin-footer"><input type="submit" class="btn-next" value="Continuar"/></div>'),
+            Field('dia', template="app/custom_components/checkbox.html"),
+            footerHtml,
         )
 class EntregaForm2(forms.ModelForm):
     class Meta:
@@ -95,8 +97,8 @@ class EntregaForm2(forms.ModelForm):
         self.helper = FormHelper(self)
         self.helper.layout = Layout(
             HTML('<button name="wizard_goto_step" class="btn-back" type="submit" value="{{ wizard.steps.prev }}"><svg width="31px" height="22px" viewBox="0 0 31 22" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><path d="M11.2306 0.421231C11.7798 0.994106 11.7798 1.94508 11.2414 2.52941L4.70545 9.50702L29.611 9.50702C30.3755 9.50702 31 10.1716 31 10.9965C31 11.8214 30.3755 12.486 29.611 12.486L4.70545 12.486L11.2522 19.4636C11.7906 20.0479 11.7798 20.9874 11.2414 21.5718C10.6923 22.1446 9.82008 22.1446 9.27093 21.5603L0.398402 12.0506C0.279958 11.9131 0.18305 11.7642 0.107676 11.5808C0.0323029 11.3975 0 11.2027 0 11.008C0 10.6184 0.139979 10.2518 0.398402 9.96532L9.27093 0.455604C9.79854 -0.140186 10.6815 -0.151643 11.2306 0.421231L11.2306 0.421231Z" id="Path" fill="#17437A" stroke="none" /> </svg></button>'),
-            InlineCheckboxes('periodo',css_class="custom-control-checkbox"),
-            HTML('<div class="footer margin-footer"><input type="submit" class="btn-next" value="Continuar"/></div>'),
+            Field('periodo', template="app/custom_components/checkbox.html"),
+            footerHtml,
         )
 class EntregaForm3(forms.ModelForm):
     class Meta:
@@ -108,9 +110,9 @@ class EntregaForm3(forms.ModelForm):
         self.helper.layout = Layout(
             HTML('<button name="wizard_goto_step" class="btn-back" type="submit" value="{{ wizard.steps.prev }}"><svg width="31px" height="22px" viewBox="0 0 31 22" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><path d="M11.2306 0.421231C11.7798 0.994106 11.7798 1.94508 11.2414 2.52941L4.70545 9.50702L29.611 9.50702C30.3755 9.50702 31 10.1716 31 10.9965C31 11.8214 30.3755 12.486 29.611 12.486L4.70545 12.486L11.2522 19.4636C11.7906 20.0479 11.7798 20.9874 11.2414 21.5718C10.6923 22.1446 9.82008 22.1446 9.27093 21.5603L0.398402 12.0506C0.279958 11.9131 0.18305 11.7642 0.107676 11.5808C0.0323029 11.3975 0 11.2027 0 11.008C0 10.6184 0.139979 10.2518 0.398402 9.96532L9.27093 0.455604C9.79854 -0.140186 10.6815 -0.151643 11.2306 0.421231L11.2306 0.421231Z" id="Path" fill="#17437A" stroke="none" /> </svg></button>'),
             HTML('<div class="card"><div class="card-body"><p class="card-heading">Próxima entrega</p><p class="card-text">{{dia|join:", "}}</p><p class="card-text">{{periodo|join:", "}}</p></div></div>'),
-            InlineRadios('preferencia',css_class="custom-control-radio"),
-            InlineRadios('frequencia',css_class="custom-control-radio"),
-            HTML('<div class="footer margin-footer"><input type="submit" class="btn-next" value="Continuar"/></div>'),
+            Field('preferencia', template="app/custom_components/radio.html"),
+            Field('frequencia', template="app/custom_components/radio.html"),
+            footerHtml,
         )
 class SaboresForm(forms.ModelForm):
     class Meta:
@@ -122,7 +124,7 @@ class SaboresForm(forms.ModelForm):
         self.helper.layout = Layout(
             # InlineCheckboxes('sabores',css_class="custom-control-checkbox"),
             Field('sabores', template="app/custom_components/multi-select.html"),
-            HTML('<div class="footer margin-footer"><input type="submit" class="btn-next" value="Continuar"/></div>'),
+            footerHtml,
         )
 
 
@@ -150,11 +152,9 @@ class CachorroForm(forms.ModelForm):
         super(CachorroForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.layout = Layout(
-            Field('nome', placeholder="Digite aqui o nome do seu cão"),
+            Field('nome', placeholder="Digite aqui o nome do seu cão", ),
             Field('sexo', template="app/custom_components/radio.html"),
-            Div(
-                Submit("submit","Continuar",css_class="btn-next"), css_class="footer margin-footer"
-            )
+            footerHtml,
         )
         # self.fields['sexo'].widget.attrs.update({'newattr' : 'attrvalue'})
 
@@ -172,7 +172,7 @@ class CachorroForm2(forms.ModelForm):
             HTML('<button name="wizard_goto_step" class="btn-back" type="submit" value="{{ wizard.steps.prev }}"><svg width="31px" height="22px" viewBox="0 0 31 22" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><path d="M11.2306 0.421231C11.7798 0.994106 11.7798 1.94508 11.2414 2.52941L4.70545 9.50702L29.611 9.50702C30.3755 9.50702 31 10.1716 31 10.9965C31 11.8214 30.3755 12.486 29.611 12.486L4.70545 12.486L11.2522 19.4636C11.7906 20.0479 11.7798 20.9874 11.2414 21.5718C10.6923 22.1446 9.82008 22.1446 9.27093 21.5603L0.398402 12.0506C0.279958 11.9131 0.18305 11.7642 0.107676 11.5808C0.0323029 11.3975 0 11.2027 0 11.008C0 10.6184 0.139979 10.2518 0.398402 9.96532L9.27093 0.455604C9.79854 -0.140186 10.6815 -0.151643 11.2306 0.421231L11.2306 0.421231Z" id="Path" fill="#17437A" stroke="none" /> </svg></button>'),
             Field('castrado', template="app/custom_components/radio.html"),
             Field('raca', placeholder="Digite aqui..."),
-            HTML('<div class="footer margin-footer"><input type="submit" class="btn-next" value="Continuar"/></div>'),
+            footerHtml,
         )
 
 class CachorroForm3(forms.ModelForm):
@@ -188,10 +188,10 @@ class CachorroForm3(forms.ModelForm):
         self.helper = FormHelper(self)
         self.helper.layout = Layout(
             HTML('<button name="wizard_goto_step" class="btn-back" type="submit" value="{{ wizard.steps.prev }}"><svg width="31px" height="22px" viewBox="0 0 31 22" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><path d="M11.2306 0.421231C11.7798 0.994106 11.7798 1.94508 11.2414 2.52941L4.70545 9.50702L29.611 9.50702C30.3755 9.50702 31 10.1716 31 10.9965C31 11.8214 30.3755 12.486 29.611 12.486L4.70545 12.486L11.2522 19.4636C11.7906 20.0479 11.7798 20.9874 11.2414 21.5718C10.6923 22.1446 9.82008 22.1446 9.27093 21.5603L0.398402 12.0506C0.279958 11.9131 0.18305 11.7642 0.107676 11.5808C0.0323029 11.3975 0 11.2027 0 11.008C0 10.6184 0.139979 10.2518 0.398402 9.96532L9.27093 0.455604C9.79854 -0.140186 10.6815 -0.151643 11.2306 0.421231L11.2306 0.421231Z" id="Path" fill="#17437A" stroke="none" /> </svg></button>'),
-            Field('nascimento'),
+            Field('nascimento', placeholder="Mês / Ano"),
             Field('pesoideal', placeholder="Ex. 10,3kg"),
             Field('peso', placeholder="Ex. 10,3kg"),
-            HTML('<div class="footer margin-footer"><input type="submit" class="btn-next" value="Continuar"/></div>'),
+            footerHtml,
         )
 
 class CachorroForm4 (forms.ModelForm):
@@ -204,7 +204,7 @@ class CachorroForm4 (forms.ModelForm):
         self.helper.layout = Layout(
             HTML('<button name="wizard_goto_step" class="btn-back" type="submit" value="{{ wizard.steps.prev }}"><svg width="31px" height="22px" viewBox="0 0 31 22" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><path d="M11.2306 0.421231C11.7798 0.994106 11.7798 1.94508 11.2414 2.52941L4.70545 9.50702L29.611 9.50702C30.3755 9.50702 31 10.1716 31 10.9965C31 11.8214 30.3755 12.486 29.611 12.486L4.70545 12.486L11.2522 19.4636C11.7906 20.0479 11.7798 20.9874 11.2414 21.5718C10.6923 22.1446 9.82008 22.1446 9.27093 21.5603L0.398402 12.0506C0.279958 11.9131 0.18305 11.7642 0.107676 11.5808C0.0323029 11.3975 0 11.2027 0 11.008C0 10.6184 0.139979 10.2518 0.398402 9.96532L9.27093 0.455604C9.79854 -0.140186 10.6815 -0.151643 11.2306 0.421231L11.2306 0.421231Z" id="Path" fill="#17437A" stroke="none" /> </svg></button>'),
             InlineRadios('atividade', template="app/custom_components/radio.html"),
-            HTML('<div class="footer margin-footer"><input type="submit" class="btn-next" value="Continuar"/></div>'),
+            footerHtml,
         )
 class CachorroForm5(forms.ModelForm):
     class Meta:
@@ -215,8 +215,8 @@ class CachorroForm5(forms.ModelForm):
         self.helper = FormHelper(self)
         self.helper.layout = Layout(
             HTML('<button name="wizard_goto_step" class="btn-back" type="submit" value="{{ wizard.steps.prev }}"><svg width="31px" height="22px" viewBox="0 0 31 22" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><path d="M11.2306 0.421231C11.7798 0.994106 11.7798 1.94508 11.2414 2.52941L4.70545 9.50702L29.611 9.50702C30.3755 9.50702 31 10.1716 31 10.9965C31 11.8214 30.3755 12.486 29.611 12.486L4.70545 12.486L11.2522 19.4636C11.7906 20.0479 11.7798 20.9874 11.2414 21.5718C10.6923 22.1446 9.82008 22.1446 9.27093 21.5603L0.398402 12.0506C0.279958 11.9131 0.18305 11.7642 0.107676 11.5808C0.0323029 11.3975 0 11.2027 0 11.008C0 10.6184 0.139979 10.2518 0.398402 9.96532L9.27093 0.455604C9.79854 -0.140186 10.6815 -0.151643 11.2306 0.421231L11.2306 0.421231Z" id="Path" fill="#17437A" stroke="none" /> </svg></button>'),
-            HTML('<div class="footer margin-footer"><input type="submit" class="btn-next" value="Continuar"/></div>'),
             Field('fisico', template="app/custom_components/radio.html"),
+            footerHtml,
         )
 class CachorroEspecialForm(forms.ModelForm):
     class Meta:
@@ -231,8 +231,8 @@ class CachorroEspecialForm(forms.ModelForm):
         self.helper = FormHelper(self)
         self.helper.layout = Layout(
             HTML('<button name="wizard_goto_step" class="btn-back" type="submit" value="{{ wizard.steps.prev }}"><svg width="31px" height="22px" viewBox="0 0 31 22" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><path d="M11.2306 0.421231C11.7798 0.994106 11.7798 1.94508 11.2414 2.52941L4.70545 9.50702L29.611 9.50702C30.3755 9.50702 31 10.1716 31 10.9965C31 11.8214 30.3755 12.486 29.611 12.486L4.70545 12.486L11.2522 19.4636C11.7906 20.0479 11.7798 20.9874 11.2414 21.5718C10.6923 22.1446 9.82008 22.1446 9.27093 21.5603L0.398402 12.0506C0.279958 11.9131 0.18305 11.7642 0.107676 11.5808C0.0323029 11.3975 0 11.2027 0 11.008C0 10.6184 0.139979 10.2518 0.398402 9.96532L9.27093 0.455604C9.79854 -0.140186 10.6815 -0.151643 11.2306 0.421231L11.2306 0.421231Z" id="Path" fill="#17437A" stroke="none" /> </svg></button>'),
-            Field('condicao', template="app/custom_components/radio.html"),
+            Field('condicao', template="app/custom_components/checkbox.html"),
             Field('medicamento', template="app/custom_components/radio.html"),
             Field('descricao', template="app/custom_components/textarea.html"),
-            HTML('<div class="footer margin-footer"><input type="submit" class="btn-next" value="Continuar"/></div>'),
+            footerHtml,
         )
