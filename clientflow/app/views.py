c85111af459d485a6affa6c11386eed11045406a
Lorenzo V <lorenzo.vella@live.com>
2020-08-19 21:23:38 -0300

    Update views.py

--- clientflow/app/views.py
+++ clientflow/app/views.py
@@ -19,7 +19,8 @@ from ipware import get_client_ip
 
 class newPasswordResetForm(PasswordResetForm):
     def send_mail(self, subject_template_name,context, from_email,
-     to_email, html_email_template_name ):
+     to_email, html_email_template_name=None ):
+
         subject = loader.render_to_string(subject_template_name, context)
         # Email subject *must not* contain newlines
         subject = ''.join(subject.splitlines())
@@ -28,6 +29,7 @@ class newPasswordResetForm(PasswordResetForm):
         email_message = EmailMultiAlternatives(subject, body, from_email, [to_email])
         html_email = loader.render_to_string('email/boas_vindas.html', context)
         email_message.attach_alternative(html_email, 'text/html')
+
         email_message.send()
 
 def calculaDescontoProgressivo(consumoKg):
