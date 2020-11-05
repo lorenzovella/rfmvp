6530b0d7be1178629afb2fc9890fc3c88614f69e
Lorenzo V <lorenzo.vella@live.com>
2020-11-05 19:47:24 -0300

    Update messagingHandler.py

--- clientflow/app/messagingHandler.py
+++ clientflow/app/messagingHandler.py
@@ -7,8 +7,8 @@ def sendMessageT(body):
     telegram_send.send(conf="telegramConf",messages=[body] )
 
 def sendMessageW(body, number):
-    account_sid = environ.get('twillio_sid')
-    auth_token = environ.get('twillio_token')
+    account_sid = environ.get('TWILIO_SID')
+    auth_token = environ.get('TWILIO_TOKEN')
     number = str('whatsapp:+55'+number)
     client = Client(account_sid, auth_token)
     message = client.messages.create(body=body,from_='whatsapp:+16106162363',to=number)
